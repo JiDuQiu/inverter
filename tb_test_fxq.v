23cea4f259fc202883c715b64ba49793dedee6cd
几度秋 <49580314+JiDuQiu@users.noreply.github.com>
2022-11-05 10:56:30 +0800

    Update tb_test_fxq.v

--- tb_test_fxq.v
+++ tb_test_fxq.v
@@ -19,8 +19,8 @@ initial begin
 end
 
 initial begin
-	$dumpfile("test.vcd");
-	$dumpvars(0, inv_tb); 
+	$dumpfile("test.vcd");//通过$dumpfile指定要保存仿真波形数据需要存储到的文件，指定保存在当前目录中test.vcd文件中（GTKWave还支持LXT,LXT2,VZT,FST和GHW等类型文件）
+	$dumpvars(0, inv_tb); //通过$dumpvars指定要保存仿真波形数据的信号，这里指定为inv_tb模块（即本模块）之下及其子模块中的所有信号，当然这个例子中没有子模块，所以仿真数据被存储的为本模块中仅有的两个信号：clk和rst_n
 end
 
 endmodule
